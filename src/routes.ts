9ac5b5a9334082cccbd1900cdd69ad8229a2dfca
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2025-08-26 22:06:45 -0300

    criado os arquivos para logica de categoria e produtos - service, controller e adicionado a rota. Agora e possivel cadastrar usuario, produto, categoria do produto e listar/buscar as infromacoes do banco de dados.

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import multer from "multer";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
+import uploadConfig from "./config/multer";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 //-- Rotas user --
 router.post('/users', new CreateUserController().handle)
@@ -16,5 +25,12 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 //-- Rotas category --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//-- Rotas product --
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router }
