03645be606c8e2950f566065b74fd931be5604ba
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2025-08-27 05:43:55 -0300

    Nessa etapa foram criados os arquivos para a logica de cadastrar e deletar um pedido(order).

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,8 @@ import { CreateProductController } from "./controllers/product/CreateProductCont
 
 import uploadConfig from "./config/multer";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 
 const router = Router();
 
@@ -32,5 +34,10 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+//-- Rotas order --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router }
